367ce7203abd2be3d6618ea5aee8d704c814c356
Thiago Luiz <thiagoluix21@gmail.com>
2023-10-17 07:35:37 -0300

    feat: created delete and done functions

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,39 +1,78 @@
 const body = document.getElementsByTagName('body')[0];
-const inputAddTodoField = body.querySelector('#inputAddTodo');
-const btnAddTodo = body.querySelector('#btnAddTodo');
-const todosContainer = body.querySelector('#todosContainer');
-
-let todos = [];
-
-document.addEventListener('DOMContentLoaded', function () {
-  fetchTodos();
-});
+const inputAddTodoField = document.getElementById('inputAddTodo');
+const btnAddTodo = document.getElementById('btnAddTodo');
+const todosContainer = document.getElementById('todosContainer');
+const btnRemoveTodo = document.getElementById('btnRemoveTodo');
 
 function fetchTodos() {
   const storedTodos = JSON.parse(localStorage.getItem('todos') || '[]');
 
+  let todoElements = '';
+
   for (const todo of storedTodos) {
-    const todoElement = document.createElement('div');
-    todoElement.className = 'todo';
-    todoElement.setAttribute('id', `${todo.id}`);
+    const todoElement = `
+      <div class="todo" id="${todo.id}">
+      <p class="text-todo ${todo.done ? 'done' : ''}" id="textTodo">${
+      todo.text
+    }</p>
+      <div class="buttons">
+        <button onclick="doneTodos(this)" class="btn-done-todo" id="doneTodo">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="feather feather-check"
+          >
+            <polyline points="20 6 9 17 4 12"></polyline>
+          </svg>
+        </button>
+        <button
+          class="btn-remove-todo"
+          id="btnRemoveTodo"
+          onclick="removeTodo(this)"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="feather feather-trash-2"
+          >
+            <polyline points="3 6 5 6 21 6"></polyline>
+            <path
+              d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"
+            ></path>
+            <line x1="10" y1="11" x2="10" y2="17"></line>
+            <line x1="14" y1="11" x2="14" y2="17"></line>
+          </svg>
+        </button>
+      </div>
+    </div>
+          `;
 
-    const paragraph = document.createElement('p');
-    paragraph.className = 'text-todo';
-    paragraph.setAttribute('id', 'textTodo');
-    paragraph.innerHTML = todo.text;
+    todoElements += todoElement;
+  }
+  todosContainer.innerHTML = todoElements;
 
-    const button = document.createElement('button');
-    button.className = 'btn-remove-todo';
-    button.setAttribute('id', 'btnRemoveTodo');
-    button.innerHTML =
-      '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash-2" > <polyline points="3 6 5 6 21 6"></polyline> <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2" ></path> <line x1="10" y1="11" x2="10" y2="17"></line> <line x1="14" y1="11" x2="14" y2="17"></line> </svg>';
-
-    todoElement.appendChild(paragraph);
-    todoElement.appendChild(button);
-
-    todosContainer.appendChild(todoElement);
+  if (todosContainer.innerHTML != '' || storedTodos == '[]') {
+    todosContainer.style.display = 'grid';
   }
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+  fetchTodos();
+});
 
 function generateId() {
   const timestamp = new Date().getTime();
@@ -48,9 +87,11 @@ function itemModel(text) {
   };
 }
 
-btnAddTodo.addEventListener('click', function (event) {
+function newTodo(event) {
   event.preventDefault();
   const inputAddTodo = inputAddTodoField.value;
+
+  if (inputAddTodo == '' || inputAddTodo.length <= 3) return;
 
   const item = itemModel(inputAddTodo);
 
@@ -60,9 +101,42 @@ btnAddTodo.addEventListener('click', function (event) {
 
   localStorage.setItem('todos', JSON.stringify(storedTodos));
 
-  todosContainer.innerHTML = '';
-
   fetchTodos();
 
   inputAddTodoField.value = '';
-});
+}
+
+function removeTodo(element) {
+  const storedTodos = JSON.parse(localStorage.getItem('todos') || '[]');
+
+  storedTodos.forEach((todo) => {
+    if (todo.id === element.parentElement.parentElement.id) {
+      const index = storedTodos.findIndex(function (todo) {
+        const paragraph =
+          element.parentElement.parentElement.children[0].innerHTML;
+
+        return todo.text === paragraph;
+      });
+
+      storedTodos.splice(index, 1);
+
+      localStorage.setItem('todos', JSON.stringify(storedTodos));
+
+      fetchTodos();
+    }
+  });
+}
+
+function doneTodos(element) {
+  const storedTodos = JSON.parse(localStorage.getItem('todos') || '[]');
+
+  storedTodos.forEach(function (todo) {
+    if (todo.id == element.parentElement.parentElement.id) {
+      return (todo.done = true);
+    }
+  });
+
+  localStorage.setItem('todos', JSON.stringify(storedTodos));
+
+  fetchTodos();
+}
